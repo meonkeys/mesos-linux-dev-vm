ee2782c2f1cd9e3f1ea9401ff136c5feba934240
Adam Monsen <haircut@gmail.com>
2013-03-29 09:42:56 -0700

    bootstrap.sh: don't try to set up g++-4.6
    Avoids this error:
    
        update-alternatives: error: alternative path /usr/bin/g++-4.6 doesn't exist.

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,7 +3,6 @@ echo START $0
 apt-get update
 apt-get -y install autoconf g++-4.4 git libcurl4-gnutls-dev libtool make openjdk-7-jdk
 update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.4 100
-update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.6 50
 update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.4 100
 update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.6 50
 update-alternatives --install /usr/bin/cpp cpp-bin /usr/bin/cpp-4.4 100
